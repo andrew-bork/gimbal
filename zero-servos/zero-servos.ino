820e12b5f941c7e7ad894367235175731945599b
bork <bork.andrew.ryan@gmail.com>
2023-09-19 23:43:28 -0700

    Created zero-servo sketch

---
+++ zero-servos/zero-servos.ino
@@ -0,0 +1,19 @@
+/** 
+ * Set all the servos to 90 degrees.
+*/
+#include <Servo.h>
+
+#define SERVO_PITCH 9
+#define SERVO_ROLL 10
+
+void setup() {
+    roll_servo.attach(SERVO_ROLL);
+    pitch_servo.attach(SERVO_PITCH);
+
+    // Center the servos.
+    roll_servo.write(90);
+    pitch_servo.write(90);
+}
+
+void loop() {
+}
